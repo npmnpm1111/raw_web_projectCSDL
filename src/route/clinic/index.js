959f15f0ce23ea163704e31cff88826ecdbfae53
npmnpm11 <npmanh.all@gmail.com>
2023-07-13 00:43:25 +0700

    ok

--- src/route/clinic/index.js
+++ src/route/clinic/index.js
@@ -2,6 +2,34 @@ const express = require('express');
 const router = express.Router();
 const db = require('../../models/index');
 
+// GET route for /giaodienphongkham/:slug
+router.get('/giaodienphongkham/:slug', async (req, res) => {
+  const slug = req.params.slug;
+  const clinic = await db.Clinic.findOne({ where: { slug: slug } });
+  if (clinic) {
+    // Render a new view for editing the clinic
+    res.render('clinic-edit', { clinic: clinic });
+  } else {
+    res.status(404).send('Clinic not found.');
+  }
+});
+// POST route for /giaodienphongkham/:slug
+router.post('/giaodienphongkham/:slug', async (req, res) => {
+  const slug = req.params.slug;
+  const { name, address, description } = req.body;
+  const clinic = await db.Clinic.findOne({ where: { slug: slug } });
+  if (clinic) {
+    // Update the clinic
+    clinic.name = name;
+    clinic.address = address;
+    clinic.description = description;
+    await clinic.save();
+    res.redirect('/clinic/' + slug);
+  } else {
+    res.status(404).send('Clinic not found.');
+  }
+});
+
 // GET route for /clinic
 router.get('/', (req, res) => {
   db.Clinic.findAll()
@@ -30,32 +58,6 @@ router.get('/:slug', (req, res) => {
       res.status(500).send('An error occurred.');
     });
 });
-// GET route for /giaodienphongkham/:slug
-router.get('/giaodienphongkham/:slug', async (req, res) => {
-  const slug = req.params.slug;
-  const clinic = await db.Clinic.findOne({ where: { slug: slug } });
-  if (clinic) {
-    // Render a new view for editing the clinic
-    res.render('clinic-edit', { clinic: clinic });
-  } else {
-    res.status(404).send('Clinic not found.');
-  }
-});
-// POST route for /giaodienphongkham/:slug
-router.post('/giaodienphongkham/:slug', async (req, res) => {
-  const slug = req.params.slug;
-  const { name, address, description } = req.body;
-  const clinic = await db.Clinic.findOne({ where: { slug: slug } });
-  if (clinic) {
-    // Update the clinic
-    clinic.name = name;
-    clinic.address = address;
-    clinic.description = description;
-    await clinic.save();
-    res.redirect('/clinic/' + slug);
-  } else {
-    res.status(404).send('Clinic not found.');
-  }
-});
+
 
 module.exports = router;
